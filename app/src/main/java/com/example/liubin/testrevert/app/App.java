2d71955fb7aca139591556d31c251f9e56bc76b8
liubin <android2upc@163.com>
2017-04-12 21:49:58 +0800

    create project

---
+++ app/src/main/java/com/example/liubin/testrevert/app/App.java
@@ -0,0 +1,15 @@
+package com.example.liubin.testrevert.app;
+
+import android.app.Application;
+
+/**
+ * Created by liubin on 2017/4/12.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
